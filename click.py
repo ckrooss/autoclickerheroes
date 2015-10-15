26132aefecd610d8cc30b3f58616d7637e559ceb
Christopher <didi2002@web.de>
2015-10-15 19:05:09 +0200

    function docstrings

--- click.py
+++ click.py
@@ -6,8 +6,6 @@ import PIL.ImageGrab as pg
 import numpy as np
 import cv2
 
-print win32api.GetCursorPos()
-
 FISH = cv2.imread("fish.png")
 
 # LEFT SCREEN
@@ -22,6 +20,10 @@ POWERS = (1001, 360)
 
 
 def find_fish():
+    """
+    Find the fish-object and return it's x and y coordinates
+    Only works on the primary monitor at 1920x1080!
+    """
     img = np.array(pg.grab())
     img = img[:, :, ::-1].copy()
     result = cv2.matchTemplate(img, FISH, method=cv2.TM_CCOEFF_NORMED)
@@ -30,6 +32,7 @@ def find_fish():
     _, maxVal, _, maxLoc = cv2.minMaxLoc(result)
 
     if maxVal:
+        # Add dirty offsets because result.shape != img.shape
         x, y = maxLoc
         y = 1080 - y
         x += 10
@@ -47,33 +50,39 @@ def click(x, y):
 
 
 def active():
+    """True if click should be continued"""
     return win32api.GetKeyState(win32con.VK_NUMLOCK)
 
 
 def do_attack():
+    """Attack once"""
     click(*ATTACK)
     sleep(1.0 / 50.0)
 
 
 def do_buy():
+    """Buy a single upgrade for the highest tier hero"""
     click(*BUY)
     sleep(1.0 / 60.0)
 
 
 def do_powers():
+    """Activate default maximum combo once"""
     for i in [1, 2, 3, 4, 5, 7, 8, 6, 9]:
         x, y = POWERS
         click(x, y + 55 * (i - 1))
-        sleep(0.1)
+        sleep(0.05)
 
 
 def click_fish():
+    """Find and click the fish-clickable"""
     x, y = find_fish()
     if x and y:
         print("Caught a fish at %s:%s" % (x, y))
         click(x, y)
         sleep(1)
 
+# TODO: Create timer based events
 for i in cycle(range(1, 1000)):
     if active():
         for _ in range(10):
