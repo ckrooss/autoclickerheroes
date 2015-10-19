f580f087e46780f2be44391d81ed7bab47bd84db
Christopher <didi2002@web.de>
2015-10-19 20:21:00 +0200

    Added logging, added a lot more autodetected sprites to templates folder

--- click.py
+++ click.py
@@ -17,33 +17,72 @@ Requirements:
 
 import win32api
 import win32con
-from time import sleep
+from time import sleep, time, strftime
 import PIL.ImageGrab as pg
 import numpy as np
 import cv2
-from threading import Timer
+from threading import Timer, RLock
+import logging
+from datetime import datetime
 
-FISH = cv2.imread("fish.png")
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
+formatter = logging.Formatter(fmt='%(asctime)s %(levelname)s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
-# LEFT SCREEN
-ATTACK = (-642, 566)
-BUY = (-1488, 834)
-POWERS = (-914, 360)
+# STDOUT
+ch = logging.StreamHandler()
+ch.setFormatter(formatter)
+ch.set_name("screen")
+ch.setLevel(logging.DEBUG)
 
-# RIGHT SCREEN
-ATTACK = (1296, 558)
-BUY = (432, 819)
-POWERS = (1001, 360)
+# LOGFILE
+fh = logging.FileHandler(datetime.fromtimestamp(time()).strftime('%Y-%m-%d_%H-%M-%S') + "_log.txt")
+fh.setFormatter(formatter)
+fh.set_name("file")
+fh.setLevel(logging.INFO)
 
-IDLE = False
+log.addHandler(ch)
+log.addHandler(fh)
+
+FISH = cv2.imread("templates/fish.png")
+BANANA = cv2.imread("templates/banana.png")
+BEE = cv2.imread("templates/bee.png")
+POWERUP = cv2.imread("templates/powerup.png")
+SKULL = cv2.imread("templates/skull.png")
+DOWN = cv2.imread("templates/down.png")
+UP = cv2.imread("templates/up.png")
+GILD = cv2.imread("templates/gild.png")
+SHOP = cv2.imread("templates/shop.png")
+UPGRADE = cv2.imread("templates/upgrade.png")
 
 CLICK_PERIOD = 1.0 / 50.0
 BUY_PERIOD = 10
 POWERS_PERIOD = 150
 FISH_PERIOD = 60
+UPGRADE_PERIOD = 300
+SEASONAL_PERIOD = 10
+
+COORDINATES = dict()
+
+scroll_lock = RLock()
 
 
-def find_object(template):
+def init_coords():
+    x, y = find_object(SHOP, "shop")
+    COORDINATES["attack"] = (x, y - 200)
+
+    x, y = find_object(UP, "up-arrow")
+    COORDINATES["up"] = (x, y)
+    COORDINATES["powerups"] = (x + 65, y - 20)
+
+    x, y = find_object(DOWN, "down-arrow")
+    COORDINATES["down"] = (x, y)
+
+    for name, (x, y) in COORDINATES.items():
+        assert x and y
+
+
+def find_object(template, name=""):
     """
     Find the object in template and return it's x and y coordinates
     Only works on the primary monitor!
@@ -62,14 +101,22 @@ def find_object(template):
         y_center = (2*y + template.shape[0]) / 2
 
         # Debug: View rectangle around template + center circle
-        # s = cv2.rectangle(img, (x, y), (x + template.shape[1], y + template.shape[0]), (0, 0, 255), 1)
-        # c = cv2.circle(s, (x_center, y_center), 22, (0, 255, 0), 1)
-        # cv2.imshow("asd", s)
-        # cv2.waitKey(0)
+        if name == "":
+            cv2.rectangle(img, (x, y), (x + template.shape[1], y + template.shape[0]), (0, 0, 255), 1)
+            cv2.circle(img, (x_center, y_center), 22, (0, 255, 0), 1)
+            cv2.imshow("asd", img)
+            cv2.waitKey(0)
 
+        # print(name, x, y)
         return x_center, y_center
     else:
         return 0, 0
+
+
+def get_pixel_values(x, y):
+    img = np.array(pg.grab())
+    img = img[:, :, ::-1].copy()
+    return img[y, x]
 
 
 def click(x, y):
@@ -84,31 +131,146 @@ def active():
 
 def do_attack():
     """Attack once"""
-    click(*ATTACK)
+    click(*COORDINATES["attack"])
     sleep(CLICK_PERIOD)
 
 
-def do_buy():
+def do_buy(hero_coords):
     """Buy a single upgrade for the highest tier hero"""
-    click(*BUY)
-    sleep(CLICK_PERIOD)
+
+    x, y = hero_coords
+
+    if x and y:
+        click(*hero_coords)
+        sleep(CLICK_PERIOD)
 
 
 def do_powers():
     """Activate default maximum combo once"""
     for i in [1, 2, 3, 4, 5, 7, 8, 6, 9]:
-        x, y = POWERS
+        x, y = COORDINATES["powerups"]
         click(x, y + 55 * (i - 1))
         sleep(CLICK_PERIOD)
 
 
 def click_fish():
     """Find and click the fish-clickable"""
-    x, y = find_object(FISH)
+    x, y = find_object(FISH, "fish")
 
     if x and y:
         click(x, y)
         sleep(CLICK_PERIOD)
+
+
+def click_bee():
+    x, y = find_object(BEE, "bee")
+
+    if x and y:
+        for _ in range(60):
+            click(x, y)
+            sleep(CLICK_PERIOD)
+
+
+def scroll_up(n=1):
+    scroll_lock.acquire()
+    up = COORDINATES["up"]
+    if n > 0:
+
+        for _ in range(n):
+            click(*up)
+            sleep(CLICK_PERIOD)
+
+        sleep(0.01 * n)
+
+    else:
+        up = up[0], up[1] + 50
+        click(*up)
+        sleep(0.3)
+
+    scroll_lock.release()
+
+
+def scroll_down(n=1):
+    scroll_lock.acquire()
+    down = COORDINATES["down"]
+    if n > 0:
+        for _ in range(n):
+            click(*down)
+            sleep(CLICK_PERIOD)
+
+        sleep(0.01 * n)
+
+    else:
+        down = down[0], down[1] - 50
+        click(*down)
+        sleep(0.3)
+
+    scroll_lock.release()
+
+
+def button_is_active(x, y):
+    # ON ~ [254 250 214]
+    # OFF ~ [0 99 69]
+    r, g, b = get_pixel_values(x, y)
+
+    return r > 100 and g > 150 and b > 100
+
+
+def search_banana(deep=False):
+    COORDINATES["banana"] = None
+
+    if not deep:
+        x, y = find_object(BANANA, "banana")
+
+        if x and y:
+            COORDINATES["banana"] = (x - 400, y)
+    else:
+        scroll_lock.acquire()
+
+        scroll_up(n=-1)
+
+        while True:
+            scroll_down(n=3)
+
+            x, y = find_object(BANANA, "banana")
+
+            if x and y:
+                COORDINATES["banana"] = (x - 400, y)
+                break
+
+            x, y = find_object(GILD, "gild")
+
+            if x and y:
+                log.info("No banana yet")
+                break
+
+        scroll_lock.release()
+
+
+def get_best_hero():
+    scroll_lock.acquire()
+
+    scroll_up(n=-1)
+    scroll_down(n=-1)
+    x, y = find_object(GILD, "GILD")
+
+    scroll_lock.release()
+
+    return (x - 20, y - 200)
+
+
+def upgrade_all():
+    scroll_lock.acquire()
+
+    scroll_up(n=-1)
+    scroll_down(n=-1)
+
+    x, y = find_object(UPGRADE, "upgrade all")
+
+    if x and y:
+        click(x, y)
+
+    scroll_lock.release()
 
 
 def attack_timer():
@@ -119,16 +281,23 @@ def attack_timer():
 
 
 def buy_timer():
-    print("buy timer ticking")
-    for _ in range(10):
-        do_buy()
+    log.info("buy timer ticking")
+    search_banana()
+
+    if COORDINATES.get("banana"):
+        target = COORDINATES["banana"]
+    else:
+        target = get_best_hero()
+
+    for _ in range(30):
+        do_buy(target)
 
     timers["buy"] = Timer(BUY_PERIOD, buy_timer)
     timers["buy"].start()
 
 
 def powers_timer():
-    print("powers timer ticking")
+    log.info("powers timer ticking")
     do_powers()
 
     timers["powers"] = Timer(POWERS_PERIOD, powers_timer)
@@ -136,24 +305,50 @@ def powers_timer():
 
 
 def fish_timer():
-    print("fish timer ticking")
+    log.info("fish timer ticking")
     click_fish()
 
     timers["fish"] = Timer(FISH_PERIOD, fish_timer)
     timers["fish"].start()
 
 
+def upgrade_timer():
+    log.info("Upgrade timer ticking")
+
+    upgrade_all()
+
+    timers["upgrade"] = Timer(UPGRADE_PERIOD, upgrade_timer)
+    timers["upgrade"].start()
+
+
+def seasonal_timer():
+    log.info("Seasonal timer ticking")
+
+    click_bee()
+
+    timers["seasonal"] = Timer(SEASONAL_PERIOD, seasonal_timer)
+    timers["seasonal"].start()
+
+
 if __name__ == '__main__':
+    init_coords()
+
+    while not active():
+        log.debug("Stand by...")
+        sleep(0.5)
+
     timers = {"attack": Timer(CLICK_PERIOD, attack_timer),
               "buy": Timer(BUY_PERIOD, buy_timer),
               "powers": Timer(POWERS_PERIOD, powers_timer),
-              "fish": Timer(FISH_PERIOD, fish_timer)}
+              "fish": Timer(3, fish_timer),
+              "upgrade": Timer(5, lambda: None),
+              "seasonal": Timer(10, seasonal_timer)}
 
     [t.start() for t in timers.values()]
 
     timers["attack"].join()
 
-    print("Canceling timers")
+    log.info("Canceling timers")
     [t.cancel() for t in timers.values()]
 
-    print("Exiting")
+    log.info("Exiting")
